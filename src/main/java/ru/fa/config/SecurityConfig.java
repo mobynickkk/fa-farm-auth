54dc14cba5d39d00aad0bbec51a80d85e4dfc680
Nikita Antropov <nvantropov13@gmail.com>
2024-09-29 14:01:29 +0300

    Init commit

---
+++ src/main/java/ru/fa/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package ru.fa.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsManager userDetailsService(DataSource dataSource) {
+        var userDetailsService = new JdbcUserDetailsManager(dataSource);
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+                .roles("USER", "ADMIN")
+                .build();
+        userDetailsService.createUser(admin);
+        return userDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.build();
+    }
+
+}
---
+++ src/main/resources/data/sql/security/create_username_index_authorities.sql
@@ -0,0 +1,1 @@
+create unique index ix_auth_username on authorities (username,authority);
